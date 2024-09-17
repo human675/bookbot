72178189ce7c5b3fe64d24167a764327bc68df54
human675 <human6754325@gmail.com>
2024-09-17 15:31:54 -0600

    11: print a report start

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ def main():
     print(word_count)
     
     lowered_string = file_contents.lower()
-    character_count{'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+    character_count = {}
+    for character in lowered_string:
+        if character in character_count:
+            character_count[character] += 1
+        else:
+            character_count[character] = 1
+
     print (character_count)
 
 main()
