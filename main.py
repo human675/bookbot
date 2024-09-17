e1746dc75b34727793b1b786545024c5aed21b0e
human675 <human6754325@gmail.com>
2024-09-17 16:58:33 -0600

    final product

--- main.py
+++ main.py
@@ -6,16 +6,28 @@ def main():
     word_count = 0
     for i in range(len(words)):
         word_count += 1
-    print(word_count)
     
     lowered_string = file_contents.lower()
     character_count = {}
     for character in lowered_string:
-        if character in character_count:
-            character_count[character] += 1
-        else:
-            character_count[character] = 1
+        if character.isalpha():
+            if character in character_count:
+                character_count[character] += 1
+            else:
+                character_count[character] = 1
+    
+    
+    print ("--- Begin report of books/frankenstein.txt ---")
+    print (f"{word_count} words found in the document")
+    print()
+    character_list = list(character_count.items())
+    character_list.sort(reverse=True, key=lambda x:x[1])
+    for i in character_list:
+        print(f'The \'{i[0]}\' character was found {i[1]} times')
+    
 
-    print (character_count)
+    print('--- End report ---')
+    
+
 
 main()
